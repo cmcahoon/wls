75919a6d7ddd3cfa54bb0f0850d65c116683a554
cmcahoon <cmcahoon@gmail.com>
2022-06-11 16:40:28 -0600

    feat: Display last accessed date.

--- src/fs.rs
+++ src/fs.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use std::io;
 use std::fs::metadata;
 use std::fs::read_dir;
@@ -15,11 +16,18 @@ pub fn recurse(path: &str) -> Result<(), io::Error> {
     for result in results {
         let entry = result?;
         let entry_path = entry.path();
+
+        // Query attributes
+        //
+        // TODO: Add some real error handling!
         let file_name = entry_path.file_name().unwrap();
-
         let file_size = get_size(entry_path.clone()).unwrap();
+        let last_accessed_time = get_last_accessed_time(entry_path.clone()).unwrap();
 
-        println!("{} {}", file_size, file_name.to_str().unwrap());
+        println!("{} {} {}",
+                 file_size,
+                 last_accessed_time,
+                 file_name.to_str().unwrap());
     }
 
     Ok(())
@@ -35,9 +43,24 @@ fn is_directory(path: &str) -> bool {
     stat.is_dir()
 }
 
-fn get_size(path: PathBuf) -> Result<u64, std::io::Error> {
-    match metadata(path) {
-        Ok(stats) => return Ok(stats.len()),
-        Err(error) => return Err(error),
+fn get_size(path: PathBuf) -> Result<u64, io::Error> {
+    return match metadata(path) {
+        Ok(stats) => Ok(stats.len()),
+        Err(error) => Err(error),
+    };
+}
+
+fn get_last_accessed_time(path: PathBuf) -> Result<String, io::Error> {
+    return match metadata(path) {
+        Ok(stats) => {
+            return match stats.accessed() {
+                Ok(last_accessed_time) => {
+                    let last_accessed_time_dt: DateTime<Utc> = last_accessed_time.into();
+                    Ok(last_accessed_time_dt.format("%b %_d %G %H:%I").to_string())
+                },
+                Err(error) => Err(error),
+            };
+        },
+        Err(error) => Err(error),
     };
 }
